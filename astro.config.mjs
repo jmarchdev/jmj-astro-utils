773430dc68a16dbcf05d1dc519dda17909579716
javier <javier.march.jarauta@cieep.com>
2024-04-16 19:49:43 +0200

    add_vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import db from "@astrojs/db";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), db()]
+  integrations: [tailwind(), react(), db()],
+  output: "server",
+  adapter: vercel()
 });
